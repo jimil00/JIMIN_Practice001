045ab4d8be6d62ce41723841338dda09f1f0a002
jimil00 <104720372+jimil00@users.noreply.github.com>
2022-05-18 17:02:47 +0900

    변수의 정의

---
+++ JIMIN_PRACTICE/src/practice001/Variable.java
@@ -0,0 +1,20 @@
+package practice001;
+
+public class Variable {
+
+	public static void main(String[] args) {
+	
+		int a = 1; // natural Number(자연수): 1,2,3,4  integer(정수): -2, -1, 0, 1, 2 
+        System.out.println(a);
+        
+        double b = 1.1; // real number(실수)-> double: -2.0, -1.0, 0, 1.0, 2.0  
+        System.out.println(b);
+        
+        String c = "Hello world";
+        System.out.println(c);
+        
+        // 각 타입에 맞는 함수를 써야 한다.
+        // int: 정수 double: 실수 String: 문자열
+	}
+
+}
