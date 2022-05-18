cab4d15f3d675d67945891fdd5a7edf3efdfe6e5
jimil00 <104720372+jimil00@users.noreply.github.com>
2022-05-19 00:38:50 +0900

    숫자와 연산

---
+++ JIMIN_PRACTICE/src/practice001/Number.java
@@ -0,0 +1,19 @@
+package practice001;
+
+public class Number {
+
+	public static void main(String[] args) {
+	// 연산자 = Operator
+		System.out.println(6+2); //8
+		System.out.println(6-2); //4
+		System.out.println(6*2); //12
+		System.out.println(6/2); //3
+		
+		System.out.println(Math.PI); //.141592653589793
+		System.out.println(Math.ceil(Math.PI));
+		System.out.println(Math.floor(Math.PI));
+		
+
+	}
+
+}
