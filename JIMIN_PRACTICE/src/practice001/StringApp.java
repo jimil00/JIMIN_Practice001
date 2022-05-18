1ecd4a0f26f1c5ec91a4d74b2b832c9324ae881e
jimil00 <104720372+jimil00@users.noreply.github.com>
2022-05-19 00:57:22 +0900

    문자열의 표현

---
+++ JIMIN_PRACTICE/src/practice001/StringApp.java
@@ -0,0 +1,18 @@
+package practice001;
+
+public class StringApp {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World"); //String 문자열
+		System.out.println('H'); // Character 문자 (한글자만 표현 가능)
+		
+		// \n: new line 줄내림
+		System.out.println("Hello \nWorld");
+		
+		// escape
+		System.out.println("Hello \"World\"");
+		
+
+	}
+
+}
