38c13c1309fc40bb9be6f8e8b8e35be15dd63c0e
jimil00 <104720372+jimil00@users.noreply.github.com>
2022-05-20 09:55:21 +0900

    변수의 효용

---
+++ JIMIN_PRACTICE/src/practice001/Letter.java
@@ -0,0 +1,16 @@
+package practice001;
+
+public class Letter {
+
+	public static void main(String[] args) {
+		String name = "egoing";
+		System.out.println("Hello, "+name+" ..."+name+" ... egoing ... bye ");
+		
+		double VAT = 10.0;
+		System.out.println(VAT);
+		
+	    
+		// 변수는 값의 이름을 부여하는 것이다. 좋은 이름을 써야한다.
+	}
+
+}
