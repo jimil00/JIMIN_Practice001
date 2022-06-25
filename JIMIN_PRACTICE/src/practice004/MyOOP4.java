2b06a68d1711e5b4c0387735ec984445f5f768e1
jimil00 <104720372+jimil00@users.noreply.github.com>
2022-06-26 06:05:46 +0900

    생성자와 this

---
+++ JIMIN_PRACTICE/src/practice004/MyOOP4.java
@@ -0,0 +1,31 @@
+package practice004;
+
+public class MyOOP4 {
+	
+	public static void main(String[] args) {
+		Print p1 =  new Print("----");
+		
+		p1.A();
+//		p1.A();
+//		p1.B();
+//		p1.B();
+		
+//		Print p2 = new Print("*****");
+		
+//		p2.A();
+//		p2.A();
+//		p2.B();
+//		p2.B();
+		
+//		p1.A();
+//		p2.A();
+//		p1.A();
+//		p2.A();
+		
+	}
+	
+}
+
+
+//생성자는 초기에 주입할 필요가 있는 값을 전달하거나 초기의 작업을 수행하도록 할 때 쓴다.
+//this는 그 클래스가 인스턴스화 되었을때 그 인스턴스를 가르키는 특수한 이름이다.
