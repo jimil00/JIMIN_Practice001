743c790164c34b1f7eef044b7b0005852e3e6d99
jimil00 <104720372+jimil00@users.noreply.github.com>
2022-07-09 02:46:58 +0900

    Boolean Datatype

---
+++ JIMIN_PRACTICE/src/practice005/BooleanApp.java
@@ -0,0 +1,21 @@
+package practice005;
+
+public class BooleanApp {
+
+	public static void main(String[] args) {
+		
+		System.out.println("One");
+		System.out.println(1);
+		
+		System.out.println(true);
+		System.out.println(false);
+		
+		String foo = "Hello World";
+		// String true = "Hello World"; -> reserved word
+		
+		System.out.println(foo.contains("world"));
+		System.out.println(foo.contains("egoing"));
+		
+	}
+
+}
