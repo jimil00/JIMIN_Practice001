d6457e40cf5ed4ee12f99dee6717722d5787a7e1
jimil00 <104720372+jimil00@users.noreply.github.com>
2022-07-09 02:47:31 +0900

    비교연산자

---
+++ JIMIN_PRACTICE/src/practice005/ComparisonOperatorApp.java
@@ -0,0 +1,14 @@
+package practice005;
+
+public class ComparisonOperatorApp {
+
+	public static void main(String[] args) {
+		
+		System.out.println(1>1);
+		System.out.println(1==1);
+		System.out.println(1<1);
+		System.out.println(1>=1);
+
+	}
+
+}
